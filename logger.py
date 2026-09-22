ca3195f9417e5a7d332a1d28e1cad687401d8d8a
Darl Cydney <darlsp17092003@hotmail.com>
2026-09-22 22:23:26 +0200

    refactor:setup of rotating file logger proxy

--- logger.py
+++ logger.py
@@ -1,33 +1,34 @@
 import logging
 from logging.handlers import RotatingFileHandler
-from pathlib import Path
-from typing import Any
+import os
 
-class UnconventionalLogger:
-    def __init__(self, log_file: str = "app.log", max_bytes: int = 1048576, backup_count: int = 3):
-        self.path = Path(log_file)
-        self.formatter = logging.Formatter('%(asctime)s | %(levelname)-8s | %(message)s')
-        self.handler = RotatingFileHandler(self.path, maxBytes=max_bytes, backupCount=backup_count)
-        self.handler.setFormatter(self.formatter)
-        self.logger = logging.getLogger('python-utils-61')
-        self.logger.setLevel(logging.DEBUG)
-        self.logger.addHandler(self.handler)
+def get_logger(name='app_logger', log_file='app.log', max_bytes=1048576, backup_count=3):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    
+    if not logger.handlers:
+        formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+        
+        file_handler = RotatingFileHandler(
+            log_file, 
+            maxBytes=max_bytes, 
+            backupCount=backup_count
+        )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+        
+    return logger
 
-    def __call__(self, msg: Any, level: int = logging.INFO) -> None:
-        self.logger.log(level, str(msg))
+class LoggerProxy:
+    def __init__(self, name):
+        self.logger = get_logger(name)
+    
+    def __getattr__(self, name):
+        return getattr(self.logger, name)
 
-def get_rotating_logger(name: str = "default") -> logging.Logger:
-    log = logging.getLogger(name)
-    if not log.handlers:
-        log.setLevel(logging.DEBUG)
-        file_handler = RotatingFileHandler("runtime.log", maxBytes=500000, backupCount=5)
-        file_handler.setFormatter(logging.Formatter('%(name)s - %(message)s'))
-        log.addHandler(file_handler)
-    return log
-
-class SingletonLogger(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super().__call__(*args, **kwargs)
-        return cls._instances[cls]
+# usage: log = LoggerProxy('main_module')
+# log.info('operation started')
