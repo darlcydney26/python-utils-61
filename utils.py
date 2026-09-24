e814add5744a0ea29695604461738056ac8f290f
Darl Cydney <darlsp17092003@hotmail.com>
2026-09-25 01:35:15 +0200

    feat:refactoring of functional utilities and registry

--- utils.py
+++ utils.py
@@ -1,46 +1,44 @@
+import collections
 import functools
-import time
 import itertools
 
-def retry_with_backoff(retries=3, delay=1):
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            for attempt in range(retries):
-                try:
-                    return func(*args, **kwargs)
-                except Exception:
-                    if attempt == retries - 1: raise
-                    time.sleep(delay * (2 ** attempt))
+def compose(*functions):
+    """functional piping mechanism for data streams"""
+    return functools.reduce(lambda f, g: lambda x: g(f(x)), functions)
+
+class Registry(collections.UserDict):
+    """dynamic decorator-based component registration system"""
+    def register(self, key):
+        def wrapper(func):
+            self[key] = func
+            return func
         return wrapper
-    return decorator
 
-def chunker(iterable, size):
-    it = iter(iterable)
-    return iter(lambda: tuple(itertools.islice(it, size)), ())
+    def execute(self, key, *args, **kwargs):
+        return self.get(key)(*args, **kwargs)
 
-def flatten(nested_iterable):
-    for item in nested_iterable:
-        if isinstance(item, (list, tuple, set)):
+def flatten(iterable):
+    """recursive transformation of nested iterables"""
+    for item in iterable:
+        if isinstance(item, (list, tuple)):
             yield from flatten(item)
         else:
             yield item
 
-def compose(*functions):
-    def inner(arg):
-        return functools.reduce(lambda acc, f: f(acc), functions, arg)
-    return inner
-
-def memoize_with_expiry(timeout=60):
-    cache = {}
+def memoize_with_ttl(ttl=60):
+    """cache implementation with expiration logic"""
     def decorator(func):
+        cache = {}
         @functools.wraps(func)
         def wrapper(*args):
+            import time
             now = time.time()
-            if args in cache and (now - cache[args]['time'] < timeout):
-                return cache[args]['value']
+            if args in cache and now - cache[args][1] < ttl:
+                return cache[args][0]
             result = func(*args)
-            cache[args] = {'value': result, 'time': now}
+            cache[args] = (result, now)
             return result
         return wrapper
     return decorator
+
+registry = Registry()
